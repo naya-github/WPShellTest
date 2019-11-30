d31c9b4f88e7e5acf21692094af70bfce69d2412
naya-github <naru2048@gmail.com>
2019-11-30 19:11:43 +0900

    develop first commit

--- funcTest.ps1
+++ funcTest.ps1
@@ -1,5 +1,5 @@
 ﻿function ABCDEFG($prm1) {
-    echo "func $prm1"
+    echo "call func $prm1"
 #        $res = $strlog -match "[A-Za-z0-9]{1,}"    # $res = は、非表示用.
 #        $strlog = $Matches[0]
 }
