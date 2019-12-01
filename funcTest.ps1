8afe5d23642c6d3ae7c213d7c88202811697feb1
naya-github <naru2048@gmail.com>
2019-12-01 12:41:30 +0900

    コメント処理＆ブランチのStartSHA１取得.

--- funcTest.ps1
+++ funcTest.ps1
@@ -1,5 +1,100 @@
-﻿function ABCDEFG($prm1) {
-    echo "call func $prm1"
-#        $res = $strlog -match "[A-Za-z0-9]{1,}"    # $res = は、非表示用.
-#        $strlog = $Matches[0]
+﻿<#
+.SYNOPSIS
+ここに概要を書きます
+
+.DESCRIPTION
+ここに説明を書きます
+
+.EXAMPLE
+ここに具体的な引数の例と解説を書きます(複数記述可)
+
+.EXAMPLE
+ここに具体的な引数の例と解説を書きます(複数記述可)
+
+.PARAMETER 引数名
+ここら引数の説明を書きます(複数記述可)
+
+.PARAMETER 引数名
+ここら引数の説明を書きます(複数記述可)
+
+PARAMETER には CommonParameters について説明が勝手に追加されるので
+「<CommonParameters> はサポートしていません」と書いておくと良いかもしれません。
+
+.LINK
+関連するリンクの URL を書きます
+http://www.vwnet.jp/Windows/PowerShell/SupportGetHelp.htm
+#>
+Set-StrictMode -Version 5.1 #Latest
+
+function global:var_dump($prm1) {
+    $prm1 | ForEach-object -Process { Write-Host $_ }
+}
+
+function global:printf {
+    if($args -is [array]) {
+        if($args.Length -ge 2) {
+            $prm1 = $args[0];
+            $prm2 = $args[1..($args.Length-1)]
+            Write-Host ($prm1 -f $prm2)
+        }
+        else {
+            Write-Host $args[0]
+        }
+    }
+    else {
+        Write-Host $args
+    }
+}
+
+
+function global:GetLatestMatchedCommitID([string]$branchName1,[string]$branchName2) {
+<#
+.SYNOPSIS
+    二つのブランチLogから一致する最新のSHA-1を１個取得.
+.DESCRIPTION
+    二つのブランチLogから一致する最新のSHA-1を１個取得.
+.EXAMPLE
+    ここに具体的な引数の例と解説を書きます(複数記述可)
+
+.PARAMETER branchName1
+    [git branch 名] RemoteName/BranchName (origin/develop)
+.PARAMETER branchName2
+    [git branch 名] RemoteName/BranchName (origin/master)
+.OUTPUTS
+    [git commit sha-1 hash] 文字列.
+#>
+    $a = git log --format=%T $branchName1
+    $b = git log --format=%T $branchName2
+    foreach( $item in $b ) {
+        $index = [Array]::IndexOf($a,$item)
+        if($index -cge 0) {
+            Write-Output $a[$index]
+            return
+        }
+    }
+    Write-Output ""
+    return
+}
+
+
+function global:GetAllMatchedCommitID([string]$branchName1,[string]$branchName2) {
+<#
+.SYNOPSIS
+    二つのブランチLogから一致するSHA-1を全て取得.
+.DESCRIPTION
+    二つのブランチLogから一致するSHA-1を全て取得.
+.EXAMPLE
+    ここに具体的な引数の例と解説を書きます(複数記述可)
+
+.PARAMETER branchName1
+    [git branch 名] RemoteName/BranchName (origin/develop)
+.PARAMETER branchName2
+    [git branch 名] RemoteName/BranchName (origin/master)
+.OUTPUTS
+    [git commit sha-1 hash] 配列.
+#>
+    $a = git log --format=%T $branchName1
+    $b = git log --format=%T $branchName2
+    $c = $b | Where-Object { $a -ccontains $_ }
+    $c | foreach{ Write-Output $_ }
 }
--- test.ps1
+++ test.ps1
@@ -10,6 +10,8 @@ Param(
 )
 . .\funcTest.ps1    # include
 
+Set-StrictMode -Version 5.1 #Latest
+
 # TEST CODE.
 $fileName = Split-Path -Leaf $ConfigFilePath
 $filePath = Split-Path -Parent $ConfigFilePath
@@ -20,28 +22,59 @@ $filePath = Split-Path -Parent $ConfigFilePath
 $hashConfig = @{}
 
 # ファイルを１行づつ読み込み処理する.
-$enc = [Text.Encoding]::GetEncoding('Shift_JIS')  # UTF8??.
+$enc = [Text.Encoding]::GetEncoding('utf-8')
 $fh = New-Object System.IO.StreamReader($ConfigFilePath, $enc)
 while (($line = $fh.ReadLine()) -ne $null) {
-    # １行の文字列を':'で区切る(最初の:でのみ区切りたい...).
-    $cell = $line -split " : "
+    # '#'始まりの行はコメント扱い.
+#    if($line -match "^#") {
+#        echo ("無視:"+$matches[0])
+#        continue
+#    }
+    # '#'以降はコメント扱い.
+    $cell = $line -split "#"
+    $line = $cell[0]
+    # １行の文字列を':'で区切る.
+    $cell = $line -split ":"
     # 設定が記載されている時.
-    if ($cell.Length -ge 2) {
-        # 空白を詰める.
+    if (($cell.Length -ge 2) -and ($cell -is [array])) {
+        # key以外は再度連結する.
+        $cell[1] = $cell[1..($cell.Length-1)] -join ":"
+        $cell[1] = $cell[1].Trim()    # 前後の空白は削除する.
+        # key文字は空白を詰める
         $cell[0] = ($cell[0] -replace " ","")
-        # 'Git Repository'は, 'Git Remote'に変更.
+        # 'Git Repository'は,'Git Remote'に変更.
         if ($cell[0] -eq 'GitRepository') {
             $cell[0] = 'GitRemote'
         }
         # 設定の配列に格納する.
-        $hashConfig.Add($cell[0], $cell[1])
+        if (-not [String]::IsNullOrWhiteSpace($cell[1])) {
+            if ($hashConfig.ContainsKey($cell[0])){
+                $hashConfig[$cell[0]] = $cell[1]
+            } else {
+                $hashConfig.Add($cell[0], $cell[1])
+            }
+        }
     }
 }
+
+printf "a{1}{0}{2}{3}!!!" "b" "c" "d" 123
+
+echo "---< sha1 >-----------------------------------"
+$c = GetAllMatchedCommitID -branchName1 "origin/develop" -branchName2 "origin/master"
+var_dump $c
+echo "---< sha1 >-----------------------------------"
+$c = GetLatestMatchedCommitID -branchName1 "origin/develop" -branchName2 "origin/master"
+var_dump $c
+
 
 if ($hashConfig.ContainsKey('GitLocalFolderRoot')) {
     echo "---< cd >-----------------------------------"
     cd $hashConfig['GitLocalFolderRoot']
     Convert-Path .
+
+    # 現在のlocal-branch
+    git rev-parse --abbrev-ref HEAD
+
     if ($hashConfig.ContainsKey('GitBranch') -and $hashConfig.ContainsKey('GitRemote')) {
 
         echo "---< git checkout >-----------------------------------"
@@ -74,8 +107,6 @@ if ($hashConfig.ContainsKey('GitLocalFolderRoot')) {
 #        git checkout -b new-branch origin/new-branch
     }
 }
-
-ABCDEFG("uhohoho!!")
 
 pause
 exit 1
