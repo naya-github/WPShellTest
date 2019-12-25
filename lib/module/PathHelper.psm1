73271e69857fe8421bb1724b4506f0da89819b68
naya-github <naru2048@gmail.com>
2019-12-25 18:59:14 +0900

    up

--- lib/module/PathHelper.psm1
+++ lib/module/PathHelper.psm1
@@ -1,4 +1,95 @@
 ﻿
+# 注意 )
+# 末尾に"\"がないとフォルダ扱いされません( $Location は修正ずみ )
+# PSDriveは扱えません
+# 例 )
+# 相対パスから絶対パスへの変換 : ConvertLocalPath "..\baz.txt" -Location "X:\foo\bar\"
+# 絶対パスから相対パスへの変換 : ConvertLocalPath "X:\foo\baz.txt" -Location "X:\foo\bar\" -Relative
+filter global:ConvertLocalPath
+{
+    Param (
+        [parameter(
+            Mandatory=$true,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true
+        )]
+        [string] $Path,
+
+        [ValidateNotNullOrEmpty()]
+        [uri] $Location = (Get-Location).Path,
+
+        [switch] $Relative
+    )
+
+    if (-not ($Location.OriginalString -match "[\\|/]{1}$")) {
+        $Location = [uri]($Location.OriginalString+"\")
+    }
+
+    if (-not $Relative) {
+        return [uri]::new($Location, $Path).LocalPath
+    }
+
+    $localPath = $Location.MakeRelative($Path) -replace '/', '\'
+    if ($localPath.StartsWith('.')) {
+        return $localPath
+    } else {
+        return ".\${localPath}"
+    }
+}
+
+
+# 相対パスから絶対パスへの変換
+function global:ToFullPath
+{
+    Param (
+        [string] $Path,
+        [string] $Root = "."
+    )
+
+    $re = Test-Path $Root
+    if ($re) {
+        Push-Location $Root
+        $re = Test-Path $Path
+        if ($re) {
+            $re = (Resolve-Path $Path).Path
+            Write-Host $re
+        } else {
+            $re = ConvertLocalPath $Path
+            Write-Host $re
+        }
+        Pop-Location
+    } else {
+        $re = ConvertLocalPath $Path -Location $Root
+        Write-Host $re
+    }
+}
+
+# 絶対パスから相対パスへの変換
+function global:ToRelativePath
+{
+    Param (
+        [string] $Path,
+        [string] $Root = "."
+    )
+    $re = Test-Path $Root
+    if ($re) {
+        Push-Location $Root
+        $re = Test-Path $Path
+        if ($re) {
+            $re = (Resolve-Path $Path -Relative)
+            Write-Host $re
+        } else {
+            $re = ConvertLocalPath $Path -Relative
+            Write-Host $re
+        }
+        Pop-Location
+    } else {
+        $re = ConvertLocalPath $Path -Location $Root -Relative
+        Write-Host $re
+    }
+}
+
+
 function global:IsDirectory([string]$path1, $endDelimiter=$true) {
     $result = $false
     if ($endDelimiter) {
@@ -18,6 +109,8 @@ function global:IsDirectory([string]$path1, $endDelimiter=$true) {
             try {
                 $ext = [System.IO.Path]::GetExtension($path1)
                 if ([bool]$ext) {
+                    $result = $false
+                } else {
                     $result = $true
                 }
             } catch [Exception] {
